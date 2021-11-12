3d071c9c1a2e3b965acf38648aa90a4824520374
zngue <zngfue@163.com>
2021-11-12 16:50:17 +0800

    add file update

--- app/router/router.go
+++ app/router/router.go
@@ -20,7 +20,7 @@ func Router(router *gin.RouterGroup) {
 		authRouter.GET("codeLink", auth.AuthLink)
 		authRouter.GET("link", auth.AuthLinkByCode)
 		authRouter.GET("authorization", auth.Authorization)
-		authRouter.GET("Webauthorization", auth.WebAuthorization)
+		authRouter.GET("webAuthorization", auth.WebAuthorization)
 	}
 
 }
